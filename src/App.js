ecf6d4aaef628f5570c96b5e72dabc04c5e02d4b
MarcialFantini <marcialfantini@gmail.com>
2022-03-25 10:34:58 -0300

    aaa

--- src/App.js
+++ src/App.js
@@ -2,27 +2,22 @@
 import './App.css';
 import HomePage from './containers/HomePage';
 
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Link
-} from "react-router-dom";
+
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 
 function App() {
   return (
-    <Router>
-      <Navbar />
 
-      <div className="App">
-        <Routes>
-          <Route path='/' element={<HomePage />} />
-        </Routes>
-      </div>
+
+
+    <div className="App">
+      <Navbar />
+      <HomePage />
       <Footer />
-    </Router>
+    </div>
+
+
   );
 }
 
